39ee9b3f4c1ee86bda20bf9efe2a10a840669831
nghia <nghiatm@airasia.com>
2026-01-15 23:32:46 +0700

    update setting

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,12 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 import keystatic from '@keystatic/astro';
-import node from '@astrojs/node';
+import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
   output: 'server',
-  adapter: node({
-    mode: 'standalone', // phổ biến nhất
-  }),
+  trailingSlash: 'never',
+  adapter: cloudflare(),
   integrations: [
     tailwind(),
     react(),
